9c56dff69aaaf1c748e858acf0423cafe023bd48
Francinaldo Almeida <fap_sb@globomail.com>
2016-03-18 16:25:43 -0500

    Change backend for delete post

--- DeletePost.php
+++ DeletePost.php
@@ -1,1 +1,11 @@
-<?php var_dump($_POST['delete']);var_dump($_POST['id']);
+<?php 
+$idsToDelete;
+if(isset($_POST['delete'])){
+	for ($i=0; $i < count($_POST['delete']); $i++) { 
+		if ($_POST['delete'][$i]=="on") {
+			array_push($idsToDelete, $_POST['id'][$i]);
+		}
+	}
+}
+echo implode(", ", $idsToDelete);
+?>
